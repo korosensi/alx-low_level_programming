39100b5da3889171bb5f53eb5a3111f775b78684
abdessalam <salim.desu@gmail.com>
2023-07-13 10:43:03 -0700

    ji

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the specified statement to the standard error.
+ *              Returns 1 to indicate an error state.
+ *
+ * Return: Always 1 (indicating an error state)
+ */
+int main(void)
+{
+    write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+    return (1);
+}

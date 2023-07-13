107f7719d2e716441188d079d786b6e5928b7978
korosensi <49047543+korosensi@users.noreply.github.com>
2023-07-13 17:35:25 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+  *main - put size of thing
+  *Return: sucsses
+*/
 int main(void)
 {
 printf("Size of char: %zu byte(s)\n", sizeof(char));
@@ -7,5 +10,5 @@ printf("Size of int: %zu byte(s)\n", sizeof(int));
 printf("Size of long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
 printf("Size of float: %zu byte(s)\n", sizeof(float));
-return 0;
+return (0);
 }

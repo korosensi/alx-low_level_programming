661e9c2282de966d96ef6ce9a31b4573d22ebca3
korosensi <49047543+korosensi@users.noreply.github.com>
2023-07-13 15:53:16 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/*hello World */
-/* main - main block
- * Return: 0 
- */
+/*hello World 
+* main - main block
+* Return: 0 
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

ffbc3a4f0f4e0c51ca90fc97578c1950acb3d490
korosensi <49047543+korosensi@users.noreply.github.com>
2023-07-15 01:07:09 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,8 @@
+
+Certainly! Here's the modified code that considers the specific values of 98 and -98:
+
+c
+Copy code
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -10,28 +15,20 @@
  */
 int main(void)
 {
-    int n;
-    int last_digit;
+    srand(time(0));  // Initialize random number generator
 
-    srand(time(0));  /* Initialize random number generator */
+    int n = (rand() % 2 == 0) ? 98 : -98;  // Assign specific values of 98 and -98
 
-    n = rand();  /* Generate a random number */
-    last_digit = n % 10;  /* Extract the last digit of the number */
+    int last_digit = abs(n) % 10;  // Extract the last digit (absolute value)
 
     printf("Last digit of %d is %d and is ", n, last_digit);
 
     if (last_digit > 5)
-    {
         printf("greater than 5\n");
-    }
     else if (last_digit == 0)
-    {
         printf("0\n");
-    }
     else
-    {
         printf("less than 6\n");
-    }
 
     return 0;
 }

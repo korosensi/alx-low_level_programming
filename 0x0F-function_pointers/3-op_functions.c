3179893c4ee2d76d133902a88843d75927e4f0b3
korosensi <49047543+korosensi@users.noreply.github.com>
2023-08-29 01:54:46 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,13 +1,11 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * op_add - args 
- * @a: is an int
- * @b: int
+ * op_add - adds two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: a+b	
+ * Return: add.
  */
 int op_add(int a, int b)
 {
@@ -15,23 +13,23 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - args 
- * @a: int
- * @b: int
+ * op_sub - subctracts two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: a - b
+ * Return: difference.
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
-/*
- * op_mul - args
- * @a: int
- * @b: int
+/**
+ * op_mul - multiplies two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- *  Return: a * b
+ * Return: multiplication.
  */
 int op_mul(int a, int b)
 {
@@ -39,11 +37,11 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - ...
- * @a: ...
- * @b: ...
+ * op_div - divides two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- *  Return: ...
+ * Return: division.
  */
 int op_div(int a, int b)
 {
@@ -52,16 +50,15 @@ int op_div(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-
 	return (a / b);
 }
 
 /**
- * op_mod - ...
- * @a: ...
- * @b: ...
+ * op_mod - calculates the module of two numbers.
+ * @a: first number.
+ * @b: second number.
  *
- * Return: ...
+ * Return: remainder of the division.
  */
 int op_mod(int a, int b)
 {
@@ -70,6 +67,5 @@ int op_mod(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-
 	return (a % b);
 }

cc46f1365f9b73770cf495cc24e131b8eb0ecca1
korosensi <49047543+korosensi@users.noreply.github.com>
2023-08-29 01:58:38 +0100

    Update 3-calc.h

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,8 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef _CALC_H_
+#define _CALC_H_
 
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct op - Struct op
  *
@@ -19,4 +21,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 #endif
